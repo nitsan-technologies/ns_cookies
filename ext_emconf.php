d70a69fa2f9e8c72be3deba1e1b370fc7d20a1d4
NITSAN Technologies <sanjay@nitsan.in>
2021-12-28 12:06:33 +0530

    [TASK] Corrected the live demo page

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Simple Cookies-Bar',
-    'description' => 'Easily install and configure simple cookie-bar to make GDPR compliant your TYPO3 site. Live-Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/cookies-hint/ You can download PRO version for more-features & free-support at https://t3terminal.com/simple-cookies-bar-typo3-extension/',
+    'description' => 'Easily install and configure simple cookie-bar to make GDPR compliant your TYPO3 site. Live-Demo: https://demo.t3terminal.com//t3t-extensions/cookies You can download PRO version for more-features & free-support at https://t3terminal.com/simple-cookies-bar-typo3-extension',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
     'state' => 'stable',
